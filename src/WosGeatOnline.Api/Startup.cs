a6d49d491727191e80a17356a03f45667ba60299
Daniel Degasperi <mrstackoverflow@hotmail.com>
2020-03-28 20:26:01 +0100

    Enabled cors

--- src/WosGeatOnline.Api/Startup.cs
+++ src/WosGeatOnline.Api/Startup.cs
@@ -26,6 +26,14 @@ namespace WosGeatOnline.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy",
+                        builder => builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader());
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +47,8 @@ namespace WosGeatOnline.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
