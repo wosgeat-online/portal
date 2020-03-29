8a726ee486c80c79e62806338bba8dcd4db16c21
Daniel Degasperi <mrstackoverflow@hotmail.com>
2020-03-29 03:13:55 +0200

    Handle images as static files

--- src/WosGeatOnline.Api/Controllers/Dto/EventDto.cs
+++ src/WosGeatOnline.Api/Controllers/Dto/EventDto.cs
@@ -13,13 +13,36 @@ namespace WosGeatOnline.Api.Controllers
 
         public string Description { get; set; }
 
-        public string Image { get; set; }
+        private string _image;
+        public string Image {
+            get {
+                return _image;
+            }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                _image = "https://wosgeatonline-api.azurewebsites.net/images/"+value;
+            }
+        }
 
         public string VideoLink { get; set; }
 
         public string AuthorName { get; set; }
 
-        public string AuthorImage { get; set; }
+        private string _authorImage { get; set; }
+        public string AuthorImage {
+            get {
+                return _authorImage;
+            }
+            set {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                _authorImage = "https://wosgeatonline-api.azurewebsites.net/images/"+value;
+            } }
 
         public int WatchCount { get; set; }
 
--- src/WosGeatOnline.Api/Startup.cs
+++ src/WosGeatOnline.Api/Startup.cs
@@ -39,6 +39,8 @@ namespace WosGeatOnline.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
